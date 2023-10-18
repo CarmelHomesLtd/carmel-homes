4d4fb4dafb4907ac77e10f965d319b273804b348
CarmelHomesLtd <142821754+CarmelHomesLtd@users.noreply.github.com>
2023-10-18 13:46:50 +0100

    fix: link to external gtag script

--- src/pages/Faq/index.jsx
+++ src/pages/Faq/index.jsx
@@ -11,13 +11,7 @@ function Faq() {
         <meta name="description" content="Find some commonly asked questions to give you more information about the Rent to Own Home Ownership Program." />
         {/*Google tag (gtag.js)*/}
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-YT7M87W0RE"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments)}
-          gtag('js', new Date());
-        
-          gtag('config', 'G-YT7M87W0RE');
-        </script>
+        <script src="global/gtag.js"></script>
       </Helmet>
       <h1 className="h-0 opacity-0">FAQs</h1>
       <Hero />
