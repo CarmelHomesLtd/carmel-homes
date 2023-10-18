6642c340ad15e0d9f541a9f054abb4311b3cc8cf
CarmelHomesLtd <142821754+CarmelHomesLtd@users.noreply.github.com>
2023-10-18 13:48:37 +0100

    fix: link to external gtag script

--- src/pages/WhatIsRentToOwn/index.jsx
+++ src/pages/WhatIsRentToOwn/index.jsx
@@ -13,13 +13,7 @@ function WhatIsRentToOwn() {
         <meta name="description" content="Rent-to-own is a home ownership program designed to make your dream of home ownership a reality. To get more details on how it works, visit this page." />
         {/*Google tag (gtag.js)*/}
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-YT7M87W0RE"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments)}
-          gtag('js', new Date());
-        
-          gtag('config', 'G-YT7M87W0RE');
-        </script>
+        <script src="global/gtag.js"></script>
       </Helmet>
       <h1 className="h-0 opacity-0">What is Rent to Own?</h1>
       <WhoCanUseRTO />
