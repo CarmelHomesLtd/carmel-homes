8b7afbffb6c4a2a42516e72d45827cf922eac5e7
CarmelHomesLtd <142821754+CarmelHomesLtd@users.noreply.github.com>
2023-10-18 12:51:33 +0100

    fix: remove semicolon

--- src/pages/ApplyNow/index.jsx
+++ src/pages/ApplyNow/index.jsx
@@ -14,7 +14,7 @@ function ApplyNow() {
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-YT7M87W0RE"></script>
         <script>
           window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
+          function gtag(){dataLayer.push(arguments)}
           gtag('js', new Date());
         
           gtag('config', 'G-YT7M87W0RE');
