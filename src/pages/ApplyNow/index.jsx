7e9b72c508e1646deae2b30a5be653d42e330d60
CarmelHomesLtd <142821754+CarmelHomesLtd@users.noreply.github.com>
2023-10-18 13:46:17 +0100

    fix: link to external gtag script

--- src/pages/ApplyNow/index.jsx
+++ src/pages/ApplyNow/index.jsx
@@ -12,13 +12,7 @@ function ApplyNow() {
         <meta name="description" content="Apply Now · Response in 24 hours · No commitment · No effect on your credit card rating." />
         {/*Google tag (gtag.js)*/}
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-YT7M87W0RE"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments)}
-          gtag('js', new Date());
-        
-          gtag('config', 'G-YT7M87W0RE');
-        </script>
+        <script src="global/gtag.js"></script>
       </Helmet>
       <h1 className="h-0 opacity-0">Apply Now</h1>
       <Benefits />
