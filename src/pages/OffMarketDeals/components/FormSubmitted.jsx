06755cac740c767cb3311d612a5d6e1bcdb5bcb7
Egwuchukwu Stephen Diala <dialaegwuchukwu@gmail.com>
2025-01-24 22:49:14 +0100

    fix: success page

--- src/pages/OffMarketDeals/components/FormSubmitted.jsx
+++ src/pages/OffMarketDeals/components/FormSubmitted.jsx
@@ -4,7 +4,7 @@ function FormSubmitted() {
       <p className="z-[2] font-bold mb-5 sm:mb-14 lg:mb-18">
         <span className="text-[1.5rem]">Application Submitted!</span>
         <br />
-        We look forward to getting you home owner ready
+        We look forward to working with you
       </p>
       <div className="bg-[url('applynow/complete.webp')] bg-center bg-contain bg-no-repeat w-full h-72 z-[2]" />
     </>
