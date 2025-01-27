5b2e388ab1841f0b02cdebfbcca1c5aa59f6bf1b
Egwuchukwu Stephen Diala <dialaegwuchukwu@gmail.com>
2025-01-27 12:15:49 +0100

    feat: adjust faqs

--- src/pages/Faq/sections/Faqs.jsx
+++ src/pages/Faq/sections/Faqs.jsx
@@ -1,11 +1,17 @@
 import { faqs, offMarketFaqs } from "../faqs";
 import FaqCard from "../components/FaqCard";
-import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function Faqs() {
+  const [active, setActive] = useState("rent-to-own")
   return (
     <section className="flex flex-col justify-center items-center gap-6 md:gap-10 px-5 py-10">
-      <Link to="/what-is-rent-to-own" className="text-[1.25rem] md:text-[1.5rem] font-bold">Rent To Own FAQs</Link>
+      <div className="flex items-center gap-3 bg-gray-100 px-1.5 py-1 rounded">
+        <button type="button" onClick={() => setActive("rent-to-own")} className={`text-lg font-semibold px-3 py-1 rounded ${active === "rent-to-own" ? "bg-white" : "bg-transparent"}`}>Rent To Own FAQs</button>
+        <button type="button" onClick={() => setActive("off-market")} className={`text-lg font-semibold px-3 py-1 rounded ${active === "off-market" ? "bg-white" : "bg-transparent"}`}>Off Market FAQs</button>
+      </div>
+      {
+        active === "rent-to-own" ? (
       <div className="h-max flex flex-col md:flex-row md:flex-wrap justify-center items-center md:items-start gap-6 md:gap-10">
         {
           faqs.map((faq, index) => (
@@ -14,9 +20,9 @@ function Faqs() {
             </div>
           ))
         }
-
       </div>
-      <Link to="/off-market-deals" className="text-[1.25rem] md:text-[1.5rem] font-bold">Off Market FAQs</Link>
+
+        ): (
       <div className="h-max flex flex-col md:flex-row md:flex-wrap justify-center items-center md:items-start gap-6 md:gap-10">
         {
           offMarketFaqs.map((faq, index) => (
@@ -25,8 +31,10 @@ function Faqs() {
             </div>
           ))
         }
-
       </div>
+            
+        )
+      }
       </section>
   )
 }
