e72e213476532db21a0daa130bff0faf0a2dee16
Egwuchukwu Stephen Diala <dialaegwuchukwu@gmail.com>
2025-03-27 10:16:15 +0100

    fix: pixel issue

--- src/pages/OffMarketForm/index.jsx
+++ src/pages/OffMarketForm/index.jsx
@@ -19,23 +19,6 @@ function OffMarketForm() {
         {/*Google tag (gtag.js)
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-YT7M87W0RE"></script>
         <script src="/public/gtag.js"></script>*/}
-        <script dangerouslySetInnerHTML={{
-          __html: `
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '1765365354322951');
-            fbq('track', 'PageView');
-          `
-        }} />
-        {/* <noscript><img height="1" width="1" style="display:none"
-        src="https://www.facebook.com/tr?id=1765365354322951&ev=PageView&noscript=1"
-        /></noscript> */}
       </Helmet>
       <div className="relative flex items-start lg:gap-40 w-full max-w-screen-2xl">
         <OffMarketBenefits />
