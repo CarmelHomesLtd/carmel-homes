0079958cbe9148448c3095a0c64f6d283716eb35
CarmelHomesLtd <142821754+CarmelHomesLtd@users.noreply.github.com>
2025-05-07 23:57:14 +0100

    chore: Send event data to Netlify endpoint

--- src/global/PixelTracker/index.js
+++ src/global/PixelTracker/index.js
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import { v4 as uuidv4 } from "uuid";
 
 function PixelTracker() {
   const location = useLocation();
@@ -8,7 +9,19 @@ function PixelTracker() {
     if (typeof fbq !== "undefined") {
       const pathsWithForms = ["/carmel-homes-contracting", "/contracting-form"];
       if (pathsWithForms.includes(location.pathname)) {
-        fbq("track", "Lead");
+        const eventID = uuidv4();
+        
+        fbq("track", "Lead", {}, {eventID});
+
+        fetch("/.netlify/functions/capi", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            event_name: "Lead",
+            event_id: eventID,
+            event_source_url: window.location.href
+          }),
+        });
       }
     }
   }, [location]);
